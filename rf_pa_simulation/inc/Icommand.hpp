e3de6c3a8b657546662c1b99552d852749c31511
LovelyV Davy <23lvdavy@gmail.com>
2026-02-23 22:36:24 +0100

    Initial file structure and phase 1

---
+++ rf_pa_simulation/inc/Icommand.hpp
@@ -0,0 +1,22 @@
+#pragma once //Prevents the header file from being included multiple times.
+# include<string> // if you want o use std::string
+
+namespace rf { //a unique folder for class name , namespace prevents name cconflicts
+
+class ICommand {
+public:
+    virtual ~ICommand() = default;
+
+    virtual std::string execute() const = 0;
+    //virtual → supports polymorphism //= 0 → pure virtual → must override
+//const → this function does NOT modify object
+
+
+};
+}
+//This fle is An interface inside a namesapce 
+//safe from multiple inclusion designed for polymorpism
+//or other words: “Any command in namespace rf must implement 
+//a function called execute() that returns a string and does not modify the object */
+
+// virtual ~ICommand() = default;:The destructor is virtual because ICommand is an interface. This ensures that when a derived class is deleted through a base class pointer, the derived class destructor is called correctly. = default tells the compiler to generate the default destructor, which is fine for an interfac
---
+++ rf_pa_simulation/src/PowerGetCommand.cpp
@@ -0,0 +1,16 @@
+#include "ICommand.hpp" // line telsl complier that you want to use this interface iconmmad
+
+namespace rf {// all the classes are goruped under one name space
+
+class PowerGetCommand : public ICommand { //defining your concrete class PowerGetCommand, which inherits from the ICommand interface. public Icommand  
+    //ans this fucntion inherting Icommand  and implement execute function
+public:
+
+    std::string execute() const override  { //const says function wont change state of the object 
+        return "[RESP] Power = 29.8 dBm";
+    }
+
+};
+
+
+}
